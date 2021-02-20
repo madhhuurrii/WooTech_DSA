4a4e1aa0a97b7b8d1815a6accffbc159d4f69dc2
Madhuri <madhuriramakrishnan061@gmail.com>
2021-02-20 20:47:08 +0530

    Function

---
+++ 2d-arrays/State_of_Wakanda2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][]arr = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        for(int k=0;k<n;k++){
+            for(int j=0;j<n;j++){
+                for(int i=0;i<n;i++){
+                    if(j-i == k){
+                        System.out.println(arr[i][j]);
+                    }
+                }
+            }
+        }
+    }
+}
